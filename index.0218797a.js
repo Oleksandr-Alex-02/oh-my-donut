a8945e58a1c90942ca925ab8eeef2cc72f149feb
Oleksandr-Alex-02 <100072859+Oleksandr-Alex-02@users.noreply.github.com>
2022-05-28 14:28:14 +0000

    Deploying to gh-pages from @ Oleksandr-Alex-02/oh-my-donuta@cb47bd64cba7d2d00bb348e8644b96b159338e81 🚀

---
+++ index.0218797a.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-mobile-open]"),closeModalBtn:document.querySelector("[data-mobile-close]"),modal:document.querySelector("[data-mobile]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.0218797a.js.map
