e41e18282f335cf7183fb3212a24a033ec4d4efe
Oleksandr-Alex-02 <100072859+Oleksandr-Alex-02@users.noreply.github.com>
2022-05-29 15:12:15 +0000

    Deploying to gh-pages from @ Oleksandr-Alex-02/oh-my-donut@ed33a64f49718dcd5a076ce127aad1b24c91438a 🚀

--- index.0218797a.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openModalBtn:document.querySelector("[data-mobile-open]"),closeModalBtn:document.querySelector("[data-mobile-close]"),modal:document.querySelector("[data-mobile]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
-//# sourceMappingURL=index.0218797a.js.map
--- index.64e57fa3.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector(".program__button"),e=document.querySelector(".program__text--visible");t.addEventListener("click",(function(){e.classList.contains("is-open")?t.textContent="Read more":t.textContent="Less",e.classList.toggle("is-open")}))}();
-//# sourceMappingURL=index.64e57fa3.js.map
