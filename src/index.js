11aff63caee126f48c71bbc3f764dbb3df988ed0
vladakalyta <vladakalyta@gmail.com>
2022-05-30 13:13:27 +0300

    about-us upd
    приховування/відображення тексту

--- src/index.js
+++ src/index.js
@@ -45,3 +45,16 @@ menuButton.addEventListener('click', () => {
   }
   headerNav.classList.toggle('is-open');
 });
+
+// копія коду Open text для About-us
+const showButton = document.querySelector('.about-us__button');
+const hiddenText = document.querySelector('.about-us__text--visible');
+showButton.addEventListener('click', () => {
+  if (hiddenText.classList.contains('is-open')) {
+   showButton.textContent = 'Read more';
+  } else {
+   showButton.textContent = 'Less';
+  }
+ hiddenText.classList.toggle('is-open');
+});
+
