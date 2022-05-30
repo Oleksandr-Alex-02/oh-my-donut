d2ef4c2d21b914ee69f23f396db9b03412d32043
StanislavMironenko <98009389+StanislavMironenko@users.noreply.github.com>
2022-05-30 11:09:15 +0300

    menu JS

--- src/index.js
+++ src/index.js
@@ -1,25 +1,47 @@
- (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-mobile-open]"),
-      closeModalBtn: document.querySelector("[data-mobile-close]"),
-      modal: document.querySelector("[data-mobile]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
+//  Open menu
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-mobile-open]'),
+    closeModalBtn: document.querySelector('[data-mobile-close]'),
+    modal: document.querySelector('[data-mobile]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+  const navLinks = document.querySelector('.nav__list');
+
+  if (navLinks) {
+    navLinks.addEventListener('click', onMenuLinkClick);
+    function onMenuLinkClick(e) {
+      e.preventDefault();
+      toggleModal();
+      const att = e.target.getAttribute('data-goto');
+      const gotoBlock = document.getElementById(att);
+
+      const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY;
+
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: 'smooth',
+      });
+
+      // }
     }
+  }
 })();
-  
-const menuButton = document.querySelector(".program__button")
-const headerNav = document.querySelector(".program__text--visible")
-menuButton.addEventListener("click", () => {
-    if (headerNav.classList.contains("is-open")) {
-        menuButton.textContent = "Read more"
-    } else {
-        menuButton.textContent = "Less"
-    }
-    headerNav.classList.toggle("is-open")
-})
+
+// Open text
+const menuButton = document.querySelector('.program__button');
+const headerNav = document.querySelector('.program__text--visible');
+menuButton.addEventListener('click', () => {
+  if (headerNav.classList.contains('is-open')) {
+    menuButton.textContent = 'Read more';
+  } else {
+    menuButton.textContent = 'Less';
+  }
+  headerNav.classList.toggle('is-open');
+});
