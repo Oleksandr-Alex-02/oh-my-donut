3312629b74679dd344f1b7a7c353a0b974b6ef28
Oleksandr-Alex-02 <oleksandr.cajkovskij.alex@gmail.com>
2022-06-03 00:02:28 +0300

    map+

--- src/index.js
+++ src/index.js
@@ -62,3 +62,20 @@ showButton.addEventListener('click', () => {
   }
   hiddenText.classList.toggle('is-open');
 });
+
+// map
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[map-data-modal-open]'),
+    closeModalBtn: document.querySelector('[map-data-modal-close]'),
+    modal: document.querySelector('[map-data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('map-is-hidden');
+  }
+})();
