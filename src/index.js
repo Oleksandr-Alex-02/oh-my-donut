92448e52ff7383e601937cf46236667e2f6f4db8
StanislavMironenko <98009389+StanislavMironenko@users.noreply.github.com>
2022-06-03 09:51:26 +0300

    Fix/map

--- src/index.js
+++ src/index.js
@@ -62,3 +62,18 @@ showButton.addEventListener('click', () => {
   }
   hiddenText.classList.toggle('is-open');
 });
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[map-data-modal-open]'),
+    closeModalBtn: document.querySelector('[map-data-modal-close]'),
+    modal: document.querySelector('[map-data-modal]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('map-is-hidden');
+  }
+})();
